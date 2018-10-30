bce3c68fe81b5fefa0900b91caf885cef2a9bfa0
AYŞE NUR ORUÇ <aysenuroruc1993@gmail.com>
2018-10-30 14:35:51 +0300

    Spring boot Zuul Commit

---
+++ SpringBootZuul/src/main/java/com/qa/main/SpringBootZuulApplication.java
@@ -0,0 +1,12 @@
+package com.qa.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootZuulApplication.class, args);
+	}
+}
